cbf6ba0b1b253354784153365a0773242844fef4
gapima <gapima7@gmail.com>
2021-11-30 13:35:18 -0300

    Pagina de validação

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st;
 
+st.write("Validador de texto")
 
-with st.form(key="include_cliente"):
-    input_name = st.text_input(label="Insira o seu nome")
-    input_age = st.number_input(label="insira sua idade")
-    input_occupation = st.selectbox("Selicone sua profissão", ["Desenvolvedor", "Musico", "Desiner", "Professor"])
-    input_button_submit = st.form_submit_button("Enviar")
+with st.form(key="include_arquivo"):
+    input_name = st.selectbox("Selicone o Banco", ["Santander", "Itau", "Bradesco"])
+    input_name = st.selectbox("Selecione o produto", ["Cobrança", "Pagamento a forncedores", "Pagamento de salarios"])
+    input_name = st.selectbox("Selecione o layout", ["240", "400"])
+    ##input_name = st.text_input(label="Insira o seu nome")
+    ##input_age = st.number_input(label="insira sua idade")
+    ##input_occupation = st.selectbox("Selicone o Banco", ["Santander", "Itau", "Bradesco"])
+    input_arquivo = st.file_uploader("Upload")
+    input_button_submit = st.form_submit_button("Processar")
+    
